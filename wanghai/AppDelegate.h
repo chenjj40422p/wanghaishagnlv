6bed3e42eb7f56895cd0d6156f48c6dbce17d9c3
apple <jinjun@uin.com>
2016-08-26 13:07:55 +0800

    init
    初始化

---
+++ wanghai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wnaghai
+//
+//  Created by apple on 16/8/26.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
